38dfbf12bb8e2880ed0d720abf408286d873614e
Tiange Xiang <tiangexiang@gmail.com>
2026-03-30 05:41:17 +0000

    update

--- unet/inf.sh
+++ unet/inf.sh
@@ -4,21 +4,18 @@ source .env
 cd "$REPO_ROOT"
 
 python unet/inf.py \
-    --checkpoint output/unet_results_gsplat/0020000.pt \
+    --checkpoint output/unet_results_gsplat/0030000.pt \
     --results_dir output/unet_inference_gsplat \
     --state_key auto \
+    --sampler ddpm \
     --class_map "$DIT_GSPLAT_CLASS_MAP" \
     --mean_file "$DIT_GSPLAT_MEAN_FILE" \
     --std_file "$DIT_GSPLAT_STD_FILE" \
     --sphere2plane_path "$DIT_GSPLAT_SPHERE2PLANE_PATH" \
     --ref_camera_tar "$DIT_GSPLAT_REF_CAMERA_TAR" \
     --render_size 512 \
-    --num_inference_steps 40 \
+    --num_inference_steps 250 \
     --num_render_views 4 \
     --seed 0 \
     --mixed_precision none \
-    --dpm_solver_order 2 \
-    --dpm_algorithm_type dpmsolver++ \
-    --dpm_solver_type midpoint \
-    --dpm_timestep_spacing trailing \
     --class_label 0
